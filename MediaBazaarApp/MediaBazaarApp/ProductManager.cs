ae98e936b3d316ff2ccc9f415d389864d9f676ab
Tanchev,Hristo H <h.tanchev@student.fontys.nl>
2021-09-16 14:51:21 +0200

    Product class/Product Manager class

---
+++ MediaBazaarApp/MediaBazaarApp/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp
+{
+    public class ProductManager
+    {
+        public List<Product> Products { get; private set; }
+        public ProductManager()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (product != null)
+            {
+                this.Products.Add(product);
+                return true;
+            }
+            else { return false; }
+        }
+
+
+    }
+}
