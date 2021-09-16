7cc01c293e28ceb8143ba1251b0f3a752c2ee5ea
Rawan <rwnalhabsi98@gmail.com>
2021-09-16 12:42:03 +0200

    implemented basic classes

---
+++ MediaBazaarApp/MediaBazaarApp/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp
+{
+    public class Department
+    {
+        private string departmentName;
+        private Employee departmentManager;
+
+        List<Employee> employees;
+
+        public Department(string departmentName, Employee departmentManager)
+        {
+            employees = new List<Employee>();
+            this.departmentManager = departmentManager;
+            this.departmentName = departmentName;
+
+        }
+
+    }
+}
---
+++ MediaBazaarApp/MediaBazaarApp/Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp
+{
+    public class Employee : User
+    {
+        private string firstName;
+        private string lastName;
+        private int BSN;
+        private string email;
+        private string gender;
+        private string firstWorkingDate;
+        private string lastWorkingDate;
+        private string birthdate;
+        private string contractType;
+        private int id;
+        private double hourlyWage;
+        private string departureReason;
+        private int shiftsPerWeek;
+        private string streetName;
+        private int streetNumber;
+        private string zipCode;
+        private string town;
+        private string country;
+        private string department;
+        private string role;
+
+        public string DepartureReason
+        {
+            get { return departureReason; }
+            set { departureReason = value; }
+        }
+        public Employee(string username,string password, string firstName, string lastName, int BSN, string email,
+            string gender, string firstWorkingDate, string lastWorkingDate, string birthdate,
+            string contractType, double hourlyWage, string streetName, int streetNumber, string zipCode, string town, string country,
+            string department, string role):base(username,password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.BSN = BSN;
+            this.email = email;
+            this.gender = gender;
+            this.firstWorkingDate = firstWorkingDate;
+            this.lastWorkingDate = lastWorkingDate;
+            this.birthdate = birthdate;
+            this.contractType = contractType;
+            this.hourlyWage = hourlyWage;
+            this.streetName = streetName;
+            this.streetNumber = streetNumber;
+            this.zipCode = zipCode;
+            this.town = town;
+            this.country = country;
+            this.department = department;
+            this.role = role;
+        }
+        public void EditDetails(string username, string password, string firstName, string lastName, int BSN, string email,
+            string gender, string firstWorkingDate, string lastWorkingDate, string birthdate,
+            string contractType, double hourlyWage, string streetName, int streetNumber, string zipCode, string town, string country,
+            string department, string role)
+        {
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.BSN = BSN;
+            this.email = email;
+            this.gender = gender;
+            this.firstWorkingDate = firstWorkingDate;
+            this.lastWorkingDate = lastWorkingDate;
+            this.birthdate = birthdate;
+            this.contractType = contractType;
+            this.hourlyWage = hourlyWage;
+            this.streetName = streetName;
+            this.streetNumber = streetNumber;
+            this.zipCode = zipCode;
+            this.town = town;
+            this.country = country;
+            this.department = department;
+            this.role = role;
+        }
+
+        public void TerminateContract(string reason,string lastWorkingDate)
+        {
+            this.DepartureReason = reason;
+            this.lastWorkingDate = lastWorkingDate;
+        }
+        public override string ToString()
+        {
+            return "ID:" + " " + this.id + " " + "Name: " + this.firstName + " " + this.lastName  + " Contract: " + this.contractType;
+        }
+    }
+}
