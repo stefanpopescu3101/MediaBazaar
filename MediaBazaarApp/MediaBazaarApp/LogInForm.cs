92c35881db39ff7f5c16549a1624f47e575e235d
Rawan <rwnalhabsi98@gmail.com>
2021-10-03 23:17:45 +0200

    shelves management done by Stefan

--- MediaBazaarApp/MediaBazaarApp/LogInForm.cs
+++ MediaBazaarApp/MediaBazaarApp/LogInForm.cs
@@ -22,7 +22,21 @@ namespace MediaBazaarApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (tbUsername.Text=="HR" || tbUsername.Text=="Department")
+            {
+                Form1 form = new Form1();
+                form.ShowDialog();
+            }
+            else if (tbUsername.Text == "Store")
+            {
+                ManipulateShelves shelves = new ManipulateShelves();
+                shelves.ShowDialog();
+            }
+            else if(tbUsername.Text == "Stock")
+            {
+                StockManager stock = new StockManager();
+                stock.ShowDialog();
+            }
 
         }
 
